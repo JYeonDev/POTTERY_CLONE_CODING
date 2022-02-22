68d48bac995d4f3ce8f6bf54419ffd7d91e5273c
JYeonDev <whddus4837@gmail.com>
2022-02-22 23:50:51 +0900

    section_item_list

---
+++ app.js
@@ -0,0 +1,8 @@
+const FILTER = document.querySelector(".filter");
+const PRODUCT_MENU = document.querySelector(".product_menu");
+
+const onClick = () => {
+  console.log("hi");
+};
+
+FILTER.addEventListener("click", onClick);
