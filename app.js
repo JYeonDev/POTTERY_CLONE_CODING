780c93185639f55d7ea824dd5c4bacf50367e6f8
JYeonDev <whddus4837@gmail.com>
2022-02-27 23:51:28 +0900

    반응형 fin

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const TEXT = document.querySelector(".text");
 const FILTER = document.querySelector(".filter");
 const PRODUCT_MENU = document.querySelector(".item_menu");
 
+const HAMBERGER = document.querySelector(".hamberger");
+const HEADER = document.querySelector("header");
+const NAV = document.querySelector(".nav_list_first");
+
 const onClick = () => {
   PRODUCT_MENU.classList.toggle("toggle");
 };
 
 FILTER.addEventListener("click", onClick);
+
+const clickEvent = () => {
+  HEADER.classList.toggle("active");
+  NAV.classList.toggle("active");
+};
+
+HAMBERGER.addEventListener("click", clickEvent);
